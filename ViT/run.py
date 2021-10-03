0ce172c18b2a3689751f13361fe682d7f7ba0765
kexiaopeng <mezereonxp@gmail.com>
2021-10-03 22:18:14 +0800

    [U] Add vscode config dir to the gitignore and fix some codes

--- ViT/run.py
+++ ViT/run.py
@@ -6,25 +6,25 @@ import query
 import torchvision
 import numpy as np
 from torch.utils.data import Dataset
-import vision_transformer
 import pathlib
+import argparse
 
-file_dir = str(pathlib.Path(__file__).parent.resolve())+'/'
+file_dir = str(pathlib.Path(__file__).parent.resolve()) + '/'
 
 
 class Small_Dataset(Dataset):
-    def __init__(self, parent_dataset,  ratio, size, shuffle_seed):
+    def __init__(self, parent_dataset, ratio, size, shuffle_seed):
         self.parent = parent_dataset
         index_array = np.arange(len(self.parent))
         self.shuffle_seed = shuffle_seed
         np.random.seed(self.shuffle_seed)
         np.random.shuffle(index_array)
         self.ratio = ratio
-        if self.ratio<0:
+        if self.ratio < 0:
             self.length = len(self.parent)
         else:
-            self.length = int(len(self.parent)*ratio)
-        if size>0 :
+            self.length = int(len(self.parent) * ratio)
+        if size > 0:
             self.length = size
 
         if self.length > len(self.parent):
@@ -33,7 +33,6 @@ class Small_Dataset(Dataset):
         self.data_list = []
         for i in range(len(self.lookup_array)):
             self.data_list.append(self.parent[self.lookup_array[i]])
-
 
     def __len__(self):
         return self.length
@@ -55,24 +54,22 @@ device = torch.device('cpu')
 model = timm.create_model('vit_base_patch16_224', pretrained=True).to(device)
 
 model.eval()
-model.oracle = query.fake_oracle(file_dir+'/profiling/profiling_result/profiling_result')
+model.oracle = query.fake_oracle(file_dir + '/profiling/profiling_result/profiling_result')
 
 img_size = 224
-normalize = torchvision.transforms.Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5])
-test_dataset = torchvision.datasets.ImageFolder(file_dir+'/ILSVRC2012',
-                torchvision.transforms.Compose([
-                    torchvision.transforms.Resize(int(img_size*8/7)),
-                    torchvision.transforms.CenterCrop(img_size),
-                    torchvision.transforms.ToTensor(),
-                    normalize,
-                ]))        
-        
-small_dataset = Small_Dataset(test_dataset,-1,100,2345)
+normalize = torchvision.transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+test_dataset = torchvision.datasets.ImageFolder(
+    file_dir + '/ILSVRC2012',
+    torchvision.transforms.Compose([
+        torchvision.transforms.Resize(int(img_size * 8 / 7)),
+        torchvision.transforms.CenterCrop(img_size),
+        torchvision.transforms.ToTensor(),
+        normalize,
+    ]))
+
+small_dataset = Small_Dataset(test_dataset, -1, 100, 2345)
 
 data_loader = torch.utils.data.DataLoader(small_dataset, batch_size=1, shuffle=False, num_workers=1)
-
-
-import argparse
 
 if __name__ == '__main__':
 
@@ -88,14 +85,13 @@ if __name__ == '__main__':
     max_duration = 0
     duration_sum = 0
 
-
-    for data,target in data_loader:
+    for data, target in data_loader:
         with torch.no_grad():
             start_time = time.time()
             output = model(data)
-            duration = time.time()- start_time
+            duration = time.time() - start_time
             # model.oracle.print_log()
-            ans = output.data.max(1,keepdim=True)[1]
+            ans = output.data.max(1, keepdim=True)[1]
             correct_num += int(ans.eq(target.data.view_as(ans)).sum())
 
             if duration > max_duration:
@@ -104,8 +100,6 @@ if __name__ == '__main__':
             duration_sum += duration
             print(correct_num, count)
             print(duration)
-            
-            
-    print('average duration: {}'.format(duration_sum/count))
+
+    print('average duration: {}'.format(duration_sum / count))
     print('maximum duration: {}'.format(max_duration))
-            
